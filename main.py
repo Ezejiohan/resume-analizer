0ea2911308bfbb9dc2649be4a5a26c6c8f2daaa9
Ezejioha Nnamdi <nnamdi@example.com>
2026-04-23 22:29:44 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import PyPDF2
+import io
+import os
+from openai import OpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+st.set_page_config(page_title="AI resume Analizer", page_icon=" ", Layout="centered")
+
